13d14a38a77ddc4672748c5ebdf7486dcd62d7a3
shoumik77again <shoumik77@vt.edu>
2025-05-20 17:29:38 -0400

    favicon

---
+++ app/components/_documents.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
